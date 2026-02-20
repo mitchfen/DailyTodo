a66355165c10cc1067c2b065580bfb42afde6747
Mitchell Fenner <mfenner@configit.com>
2026-02-20 16:22:21 -0500

    fix warnings (#6)

--- TodoItem.cs
+++ TodoItem.cs
@@ -2,7 +2,7 @@ namespace DailyTodo
 {
     public class TodoItem
     {
-        public string Text { get; set; }
+        public string Text { get; set; } = string.Empty;
         public bool IsCompleted { get; set; }
     }
 }
